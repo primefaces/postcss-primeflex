52c8865161e44ddfeec789a46fc2ee5f74d8660f
Cagatay Civici <cagatay.civici@gmail.com>
2023-07-26 22:34:54 +0300

    Implemented new utilities

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ const grid = require('./lib/grid');
 const display = require('./lib/display');
 const defaultOptions = require('./lib/options');
 const typography = require('./lib/typography');
+const flexbox = require('./lib/flexbox');
 const elevation = require('./lib/elevation');
+const image = require('./lib/image');
+const position = require('./lib/position');
+const overflow = require('./lib/overflow');
+const zindex = require('./lib/zindex');
+const userselect = require('./lib/userselect');
+const liststyle = require('./lib/liststyle');
+const misc = require('./lib/misc');
+const transition = require('./lib/transition');
+const transform = require('./lib/transform');
 
 module.exports = (opts = {}) => {
     return {
@@ -11,9 +21,27 @@ module.exports = (opts = {}) => {
             primeflex: (atRule) => {
                 const _opts = {...defaultOptions, ...opts};
                 grid(atRule, _opts);
+                //colors
+                //formlayout
                 display(atRule, _opts);
                 typography(atRule, _opts);
+                flexbox(atRule, _opts);
+                //gap
+                //spacing
                 elevation(atRule, _opts);
+                //border
+                //borderradius
+                //size
+                position(atRule, _opts);
+                overflow(atRule, _opts);
+                zindex(atRule, _opts);
+                image(atRule, _opts);
+                userselect(atRule, _opts);
+                liststyle(atRule, _opts);
+                misc(atRule, _opts);
+                transition(atRule, _opts);
+                transform(atRule, _opts);
+                //animation
                 atRule.remove();
             }
         }
